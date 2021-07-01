75137b065c52795206e4e6f289e74a3eb9c68126
Sridhar Sarnobat <srsarnob@cisco.com>
2021-06-30 20:30:14 -0700

    Minor changes (untested)

---
+++ jdk9/src/main/java/net/petrikainulainen/gradle/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.petrikainulainen.gradle;
+
+/**
+ * @author Petri Kainulainen
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
