dc16801473becedec7c174eff782dab6aa491c77
Sridhar Sarnobat <srsarnob@cisco.com>
2021-07-01 01:06:28 -0700

    Minor changes (untested)

--- jdk9/run.sh
+++ jdk9/run.sh
@@ -2,3 +2,5 @@ JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.1.jdk/Contents/Home GRADLE_
 JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.1.jdk/Contents/Home GRADLE_HOME=/Volumes/Apps/gradle-6.8.1 ./gradlew jlink
 #build/image/bin/hello
 ./build/image/bin/java -m org.beryx.modular.hello/org.beryx.modular.hello.Hello
+
+# TODO: how to launch using jaotc? Read https://www.javacodegeeks.com/2020/10/using-java-9-modularization-to-ship-zero-dependency-native-apps.html
