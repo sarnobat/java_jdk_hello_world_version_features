a01293f22fb960021be344f5900784db2c2d23d0
Sridhar Sarnobat <srsarnob@cisco.com>
2021-07-01 00:22:42 -0700

    Minor changes (untested)

--- jdk9_jlink/run.sh
+++ jdk9_jlink/run.sh
@@ -1,2 +1,3 @@
-JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.1.jdk/Contents/Home GRADLE_HOME=/Volumes/Apps/gradle-6.8.1 gradle build
-build/scripts/jdk9_jlink
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.1.jdk/Contents/Home GRADLE_HOME=/Volumes/Apps/gradle-6.8.1 ./gradlew run
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.1.jdk/Contents/Home GRADLE_HOME=/Volumes/Apps/gradle-6.8.1 ./gradlew jlink
+build/image/bin/hello
--- jdk9_jlink/src/main/java/org/beryx/modular/hello/Hello.java
+++ jdk9_jlink/src/main/java/org/beryx/modular/hello/Hello.java
@@ -7,6 +7,6 @@ public class Hello {
 	private static final Logger logger = LoggerFactory.getLogger(Hello.class);
 
 	public static void main(String[] args) {
-		logger.info("Hello, modular Java!");
+		logger.info("Hello, modular Java");
 	}
 }
