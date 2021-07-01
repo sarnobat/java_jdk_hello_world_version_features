7e64442d5a29d650d99aff460bc992bbc1bf668c
Sridhar Sarnobat <srsarnob@cisco.com>
2021-07-01 01:03:00 -0700

    Minor changes (untested)

--- jdk9/run.sh
+++ jdk9/run.sh
@@ -1,3 +1,4 @@
 JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.1.jdk/Contents/Home GRADLE_HOME=/Volumes/Apps/gradle-6.8.1 ./gradlew run
 JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.1.jdk/Contents/Home GRADLE_HOME=/Volumes/Apps/gradle-6.8.1 ./gradlew jlink
-build/image/bin/hello
+#build/image/bin/hello
+./build/image/bin/java -m org.beryx.modular.hello/org.beryx.modular.hello.Hello
