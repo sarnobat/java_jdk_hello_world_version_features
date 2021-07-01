12da4fa5bf27f8d0656b6931f3bd76da297daa54
Sridhar Sarnobat <srsarnob@cisco.com>
2021-07-01 00:24:19 -0700

    Minor changes (untested)

--- jdk9/src/main/java/org/beryx/modular/hello/Hello.java
+++ jdk9/src/main/java/org/beryx/modular/hello/Hello.java
@@ -3,6 +3,7 @@ package org.beryx.modular.hello;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// https://github.com/beryx-gist/badass-jlink-example
 public class Hello {
 	private static final Logger logger = LoggerFactory.getLogger(Hello.class);
 
