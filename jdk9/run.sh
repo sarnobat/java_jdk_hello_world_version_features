047f83a057408eff9fce4a9277d014813e4c0ad0
Sridhar Sarnobat <srsarnob@cisco.com>
2021-07-02 20:13:05 -0700

    Minor changes (untested)

--- jdk9/run.sh
+++ jdk9/run.sh
@@ -1,8 +1,10 @@
 JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.1.jdk/Contents/Home GRADLE_HOME=/Volumes/Apps/gradle-6.8.1 ./gradlew run
 JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.1.jdk/Contents/Home GRADLE_HOME=/Volumes/Apps/gradle-6.8.1 ./gradlew jlink
-#build/image/bin/hello
+
+# I don't like this. I just want to execute a file without passing arguments
 ./build/image/bin/java -m org.beryx.modular.hello/org.beryx.modular.hello.Hello
 
+# The "hello" shell script (or should I say "hell script") isn't named with an extension, and you must be in the right dir to execute it. Pretending it's like a binary is counterproductive if you can't run it from any dir.
 cd ./build/image/bin/
 hello
 
